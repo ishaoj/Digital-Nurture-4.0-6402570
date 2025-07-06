e4d208152df8638b8a35d12117eb78fa4567e14d
Isha Ojha <ishaojha41@gmail.com>
2025-07-06 15:09:36 +0530

    Exercise 2

--- Week3/Library Management/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.isha.library.repository;
-
-public class BookRepository {
-    public void printAllBooks() {
-        System.out.println("Listing all books from the repository...");
-    }
-}
