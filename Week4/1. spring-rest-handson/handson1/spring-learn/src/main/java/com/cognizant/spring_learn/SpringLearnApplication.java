03e5def4a668acffa24e1b5c501583f6befef2b8
Isha Ojha <ishaojha41@gmail.com>
2025-07-21 13:10:43 +0530

    Modified week 2 outputs

--- Week4/1. spring-rest-handson/handson1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ Week4/1. spring-rest-handson/handson1/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,6 +1,7 @@
 package com.cognizant.springlearn;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.text.SimpleDateFormat;
@@ -8,19 +9,28 @@ import java.util.Date;
 
 public class SpringLearnApplication {
 
+    // Step 1: Logger setup
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
     public static void displayDate() {
+        LOGGER.info("START");
+
         ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
         SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+
         try {
             Date date = format.parse("31/12/2018");
-            System.out.println("Parsed Date: " + date);
+            LOGGER.debug("Parsed Date: {}", date);
         } catch (Exception e) {
-            System.out.println("Error parsing date: " + e.getMessage());
+            LOGGER.error("Error parsing date: {}", e.getMessage());
         }
+
+        LOGGER.info("END");
     }
 
     public static void main(String[] args) {
+        LOGGER.info("Application START");
         displayDate();  // Call the method to test output
+        LOGGER.info("Application END");
     }
 }
-
