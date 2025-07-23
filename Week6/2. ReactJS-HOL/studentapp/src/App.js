d67fe7b79ec6b584175eb686fc9a031f933447bc
Isha Ojha <ishaojha41@gmail.com>
2025-07-23 10:21:12 +0530

    Exercise 2 of Week 6

---
+++ Week6/2. ReactJS-HOL/studentapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+import Home from "./Components/Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
