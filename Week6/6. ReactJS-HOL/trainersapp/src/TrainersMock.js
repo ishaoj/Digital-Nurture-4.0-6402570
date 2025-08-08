0812ff7bce7ec6fd4160f02538a9129a1b704345
Isha Ojha <ishaojha41@gmail.com>
2025-08-08 16:42:29 +0530

    Exercises Done

---
+++ Week6/6. ReactJS-HOL/trainersapp/src/TrainersMock.js
@@ -0,0 +1,21 @@
+import Trainer from "./Trainer";
+const trainersMock = [
+    new Trainer('t-syed8',
+        'Syed Khaleelullah@cognizant.com',
+        'khaleelullah@cognizant.com',
+        '976765169662',
+        '.NET',
+        ['C#', 'SQL Server', 'React','.NET Core']),
+    new Trainer('t-syed8',
+        'Syed Khaleelullah@cognizant.com',
+        'khaleelullah@cognizant.com',
+        '976765169662',
+        '.NET',
+        ['C#', 'SQL Server', 'React','.NET Core']),
+    new Trainer('t-syed8',
+        'Syed Khaleelullah@cognizant.com',
+        'khaleelullah@cognizant.com',
+        '976765169662',
+        '.NET',
+        ['C#', 'SQL Server', 'React','.NET Core'])
+]
